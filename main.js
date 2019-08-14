c0ed63e8181a14597b5abce437d3bcb664a00c58
Guillaume Charmetant <cguille.dev@gmail.com>
2019-08-14 21:48:13 +0200

    Select scribble to display by day

--- main.js
+++ main.js
@@ -1,19 +1,20 @@
-'use strict';
+import Day from '/lib/day.js';
 
 const scribbler = document.querySelector('#scribbler');
-const scribbleDateDisplay = document.querySelector('#scribble-date');
-const today = dateParts(new Date);
+const scribbleDateInput = document.querySelector('#scribble-date');
+const today = Day.today();
 const todaysKey = scribbleKey(today);
-const todaysRecord = localStorage.getItem(todaysKey);
 
-scribbleDateDisplay.textContent = `${today.year}-${today.month}-${today.day}`;
+loadScribbleFor(today, { init: true });
 
-if (todaysRecord !== null) {
-    const scribble = JSON.parse(todaysRecord);
-    scribbler.value = scribble.content || scribble.innerText;
-}
+scribbleDateInput.value = today.format({ separator: '-' });
+scribbleDateInput.max = scribbleDateInput.value;
 
 scribbler.addEventListener('keyup', debounce(800, function saveScribblerOnChange() {
+    if (scribbler.hasAttribute('readonly')) {
+        return;
+    }
+
     const scribble = { content: scribbler.value.trim() };
     const record = JSON.stringify(scribble);
 
@@ -22,21 +23,47 @@ scribbler.addEventListener('keyup', debounce(800, function saveScribblerOnChange
 
 scribbler.focus();
 
-function scribbleKey(dateParts) {
-    return `scribble_of_${dateParts.year}_${dateParts.month}_${dateParts.day}`;
-}
-
-function dateParts(date) {
-    let year = date.getFullYear().toString();
-
-    let month = (date.getMonth() + 1).toString();
-    if (month.length === 1) {
-        month = '0' + month;
+scribbleDateInput.addEventListener('change', () => {
+    if (!scribbleDateInput.valueAsDate) {
+        return;
     }
 
-    let day = date.getDate().toString();
+    const selectedDay = Day.fromDate(scribbleDateInput.valueAsDate);
+    const selectedToday = selectedDay.equals(today);
+    loadScribbleFor(selectedDay, { readOnly: !selectedToday });
+});
 
-    return { year: year, month: month, day: day };
+function loadScribbleFor(day, options = {}) {
+    const key = scribbleKey(day);
+    const record = localStorage.getItem(key);
+
+    if (record === null) {
+        if (options.init) {
+            const scribble = { content: '' };
+            const record = JSON.stringify(scribble);
+
+            localStorage.setItem(key, record);
+        } else {
+            scribbler.value = "No scribble this day…";
+            scribbler.setAttribute('readonly', true);
+        }
+
+        return;
+    }
+
+    const scribble = JSON.parse(record);
+
+    if (options.readOnly) {
+        scribbler.setAttribute('readonly', true);
+    } else {
+        scribbler.removeAttribute('readonly');
+    }
+
+    scribbler.value = scribble.content;
+}
+
+function scribbleKey(day) {
+    return `scribble_of_${day.format({ separator: '_' })}`;
 }
 
 function debounce(delay, fn) {
