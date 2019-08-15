2bc2d06545704f610a53aa460fc1689802118b23
Guillaume Charmetant <cguille.dev@gmail.com>
2019-08-15 16:03:15 +0200

    Add more tests to Day class

--- tests/lib/day.test.js
+++ tests/lib/day.test.js
@@ -4,6 +4,10 @@ import Day from '../../docs/lib/day.js';
 export {
     testFromDate,
     testToday,
+    testFormat,
+    testEquals,
+    testToDate,
+    testValueOf,
 };
 
 testFromDate.description = 'Day.fromDate() works';
@@ -24,4 +28,41 @@ function testToday() {
     assert.strictEqual(today.year, date.getFullYear(), "today's year is the full year of today's Date");
     assert.strictEqual(today.month, date.getMonth() + 1, "today's month corresponds to the month of today's Date");
     assert.strictEqual(today.day, date.getDate(), "today's day is the date of today's Date");
+}
+
+testFormat.description = 'day.format() works';
+function testFormat() {
+    let day;
+
+    day = new Day(2019, 11, 23);
+    assert.strictEqual(day.format({ separator: '-' }), '2019-11-23', "day formatting can use '-' as a separator");
+    assert.strictEqual(day.format({ separator: '_' }), '2019_11_23', "day formatting can use '_' as a separator");
+
+    day = new Day(2019, 8, 1);
+    assert.strictEqual(day.format({ separator: '-' }), '2019-08-01', "day formatting ensures months and days are two digits");
+}
+
+testEquals.description = 'day.equals() works';
+function testEquals() {
+    const day1 = new Day(2019, 8, 1);
+    const day2 = Day.fromDate(new Date('2019-08-01'));
+    const day3 = new Day(2019, 8, 2);
+
+    assert(day1.equals(day2), "two instances of the same day are equal");
+    assert(!day2.equals(day3), "two instances of different days are not equal");
+}
+
+testToDate.description = 'day.toDate() works';
+function testToDate() {
+    const day = new Day(2019, 8, 1);
+    const date = new Date('2019-08-01');
+
+    assert.notStrictEqual(day.toDate(), date, "the conversion of a day to a date works");
+}
+
+testValueOf.description = 'day.valueOf() works';
+function testValueOf() {
+    const day = new Day(2019, 8, 1);
+
+    assert.strictEqual(day.valueOf(), day.toDate().valueOf(), "the valueOf a Day is the same as its Date counterpart");
 }
