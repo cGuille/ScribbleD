4afa055b9fce11723ec5142538ab9a5468bdc1f8
Guillaume Charmetant <cguille.dev@gmail.com>
2019-08-15 14:33:48 +0200

    Do not test the constructor
    I am not sure it is needed to test the class constructor. It will be
    used by other tests anyway.

--- tests/lib/day.new.test.js
+++
@@ -1,6 +0,0 @@
-import assert from 'assert';
-import Day from '../../docs/lib/day.js';
-
-export default function () {
-    assert(new Day(2019, 8, 14) instanceof Day, 'day can be constructed with year, month and day');
-}
